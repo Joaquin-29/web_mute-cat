33e99447fcde1d0524b8dd40c7e49f0db0394188
Joaquin-29 <joacostruyas@gmail.com>
2024-08-03 16:53:19 -0300

    x

--- js/navbar.js
+++ js/navbar.js
@@ -12,21 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(() => {
             document.body.classList.remove('hidden');
             document.body.classList.add('visible');
-        }, 100);
-    });
-
-    const links = document.querySelectorAll('a');
-
-    links.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            const href = this.href;
-
-            document.body.classList.add('fade-out');
-
-            setTimeout(() => {
-                window.location.href = href;
-            }, 1000);
-        });
+        }, 50);
     });
 });
