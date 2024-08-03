5cc9324ac219b6fddd257cec6a1c25fe35d2e185
Joaquin-29 <joacostruyas@gmail.com>
2024-08-03 16:25:32 -0300

    Add common.cs

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('navbar-toggle');
-    const navbarMenu = document.getElementById('navbar-menu');
+    $("#navbar").load("navbar.html", function () {
+        const toggleButton = document.getElementById('navbar-toggle');
+        const navbarMenu = document.getElementById('navbar-menu');
 
-    toggleButton.addEventListener('click', function () {
-        navbarMenu.classList.toggle('show');
+        if (toggleButton && navbarMenu) {
+            toggleButton.addEventListener('click', function () {
+                navbarMenu.classList.toggle('show');
+            });
+        }
+
+        setTimeout(() => {
+            document.body.classList.remove('hidden');
+            document.body.classList.add('visible');
+        }, 100);
+    });
+
+    const links = document.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const href = this.href;
+
+            document.body.classList.add('fade-out');
+
+            setTimeout(() => {
+                window.location.href = href;
+            }, 1000);
+        });
     });
 });
